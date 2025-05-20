168a3e41e23c82435fd35d9b17511cd19d553c95
HemangiKaidal <147647542+HemangiKaidal@users.noreply.github.com>
2025-05-20 20:20:27 +0530

    Hello world

--- src/Lab_001_Helloworld.java
+++
@@ -1,5 +0,0 @@
-public class Lab_001_Helloworld {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- src/Lab_002_Test2.java
+++
@@ -1,5 +0,0 @@
-public class Lab_002_Test2 {
-    public static void main(String[] args) {
-        System.out.println("ABC");
-    }
-}
